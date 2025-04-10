275f874fb4f11dca2a51846c58b7743088d33a1d
Paramon2003 <fgqwertyui1234@yandex.ru>
2025-04-10 20:03:06 +0300

    added edit replicas window

--- app/src/main/java/ui/EditController.java
+++ app/src/main/java/ui/EditController.java
@@ -1,14 +1,84 @@
 package ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class EditController {
     @FXML
-    private Label welcomeText;
+    private VBox textAreaContainer;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX!");
+    public void initialize() {
+        List<Speaker> speakers = List.of(
+                new Speaker("Анна", "/images/logo.png"),
+                new Speaker("Борис", "/images/UserSpeak.png"),
+                new Speaker("Виктор", "/images/UserSpeak2.png"),
+                new Speaker("Галина", "/images/DangerCircle.png")
+        );
+        int replicas = 5;
+        for (int i = 0; i < replicas; i++) {
+            ComboBox<Speaker> comboBox = new ComboBox<>();
+            comboBox.getItems().addAll(speakers);
+
+            comboBox.setCellFactory(lv -> new ListCell<>() {
+                private final ImageView imageView = new ImageView();
+                {
+                    imageView.setFitHeight(24);
+                    imageView.setFitWidth(24);
+                }
+
+                @Override
+                protected void updateItem(Speaker item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty || item == null) {
+                        setGraphic(null);
+                        setText(null);
+                    } else {
+                        imageView.setImage(new Image(getClass().getResourceAsStream(item.getImagePath())));
+                        setGraphic(imageView);
+                        setText(item.getName());
+                    }
+                }
+            });
+
+            comboBox.setButtonCell(new ListCell<>() {
+                private final ImageView imageView = new ImageView();
+                {
+                    imageView.setFitHeight(24);
+                    imageView.setFitWidth(24);
+                }
+
+                @Override
+                protected void updateItem(Speaker item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty || item == null) {
+                        setGraphic(null);
+                        setText(null);
+                    } else {
+                        imageView.setImage(new Image(getClass().getResourceAsStream(item.getImagePath())));
+                        setGraphic(imageView);
+                        setText(item.getName());
+                    }
+                }
+            });
+
+
+            VBox.setMargin(comboBox, new javafx.geometry.Insets(0, 0, 5, 0));
+
+            textAreaContainer.getChildren().add(comboBox);
+            TextArea textArea = new TextArea();
+            textArea.setText("meimviece");
+            textArea.setPrefRowCount(3);
+            VBox.setMargin(textArea, new javafx.geometry.Insets(0, 0, 50, 0));
+            textAreaContainer.getChildren().add(textArea);
+        }
     }
 }
--- app/src/main/java/ui/EditWindow.java
+++ app/src/main/java/ui/EditWindow.java
@@ -3,24 +3,23 @@ package ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class EditWindow extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                EditWindow.class.getResource("/fx_screens/EditView.fxml")
-        );
-
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(EditWindow.class.getResource("EditView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        //stage.setTitle("Извлечение аудио");
+        //stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
