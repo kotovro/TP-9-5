69b00f5ea3a39a2646ebf57a80a6022149411d6b
kotovro <kotvoro@gmail.com>
2025-04-10 20:25:46 +0300

    feature/text-editor Refactors Transcript and FindAction so that text search is responsibility of of Transcript

--- app/src/main/java/logic/general/Transcript.java
+++ app/src/main/java/logic/general/Transcript.java
@@ -1,8 +1,10 @@
 package logic.general;
 
+import javafx.util.Pair;
 import logic.text_edit.Buffer;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Transcript {
@@ -18,4 +20,19 @@ public class Transcript {
     public void cutReplica(int index, Buffer buffer) {
 
     }
+
+    List<Pair<Replica, Integer>> searchResults = new LinkedList<>();
+    List<Pair<Replica, Integer>> getSearchResults() {
+        return searchResults;
+    }
+
+    public List<Pair<Replica, Integer>> findText(Transcript transcript, String searchText)
+    {
+        for (Replica replica : transcript.getReplicas()) {
+            Integer searchedTextIndex =  replica.getText().indexOf(searchText);
+            searchResults.add(new Pair<>(replica, searchedTextIndex));
+        }
+        return searchResults;
+    }
+
 }
