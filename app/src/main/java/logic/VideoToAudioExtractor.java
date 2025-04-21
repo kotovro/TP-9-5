e9d2806063ac42f888a614ffd7025c3fcb58d159
blacklavilass <vita20041129@yandex.ru>
2025-04-21 11:09:43 +0300

    restuctured to actual condition

---
+++ app/src/main/java/logic/VideoToAudioExtractor.java
@@ -0,0 +1,42 @@
+package logic;
+
+import java.io.File;
+import java.io.IOException;
+
+public class VideoToAudioExtractor {
+
+    private static String findFFmpeg() {
+        String[] paths = System.getenv("PATH").split(File.pathSeparator);
+        for (String path : paths) {
+            File ffmpeg = new File(path, "ffmpeg.exe");
+            if (ffmpeg.exists() && ffmpeg.canExecute()) {
+                return ffmpeg.getAbsolutePath();
+            }
+        }
+        return null;
+    }
+
+    public static void extractAudio(File video, String audioPath) {
+        String ffmpegPath = findFFmpeg();
+        if (ffmpegPath != null) {
+            String command = String.format("\"%s\" -i \"%s\" -vn -acodec libmp3lame \"%s\"",
+                    ffmpegPath, video.getAbsolutePath(), audioPath);
+            try {
+                ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+                processBuilder.inheritIO();
+                Process process = processBuilder.start();
+                int exitCode = process.waitFor();
+                System.out.println("Audio extracted from " + video.getAbsolutePath());
+                if (exitCode == 0) {
+                    System.out.println("Audio extracted by Path: " + audioPath);
+                } else {
+                    System.out.println("Error: " + exitCode);
+                }
+            } catch (IOException | InterruptedException e) {
+                System.out.println("Error with audio extraction: " + e.getMessage());
+            }
+        } else {
+            System.out.println("FFmpeg hasn't been found");
+        }
+    }
+}
---
+++ app/src/test/java/logic/AppTest.java
@@ -0,0 +1,13 @@
+package logic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
