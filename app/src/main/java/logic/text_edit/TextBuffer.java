f0b0da5896af3e26a2f3c3a710a27f7a106a5696
kotovro <kotvoro@gmail.com>
2025-04-22 18:34:53 +0300

    feature/text-editor Resolves some issues, adds buffer for replica and text

---
+++ app/src/main/java/logic/text_edit/TextBuffer.java
@@ -0,0 +1,33 @@
+package logic.text_edit;
+
+import java.awt.*;
+import java.awt.datatransfer.*;
+import java.io.IOException;
+
+/// here we can store some info that was acquired via ctrl+x/ctrl+v
+public class TextBuffer {
+    private static Clipboard clipboardForString = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+    public static boolean isEmpty() {
+        try {
+            Transferable contents = clipboardForString.getContents(null);
+            return (contents == null) || !contents.isDataFlavorSupported(DataFlavor.stringFlavor);
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    public static void setValue(String selectedString) {
+        StringSelection stringSelection = new StringSelection(selectedString);
+        clipboardForString.setContents(stringSelection, null);
+    }
+
+    public static String getValue() {
+        try {
+            return (String) clipboardForString.getContents(null).getTransferData(DataFlavor.stringFlavor);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
