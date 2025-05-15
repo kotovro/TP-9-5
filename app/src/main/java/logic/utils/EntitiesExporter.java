6a36718ccf3fae3200a3b6d9ce15798e989a40fb
kotovro <kotvoro@gmail.com>
2025-05-15 22:28:30 +0300

    feature/text-export Enables text export possibility

--- app/src/main/java/logic/utils/EntitiesExporter.java
+++ app/src/main/java/logic/utils/EntitiesExporter.java
@@ -1,9 +1,12 @@
 package logic.utils;
 
+import logic.general.Protocol;
 import logic.general.Replica;
 import logic.general.Transcript;
+import logic.persistence.dao.TranscriptDao;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -20,6 +23,20 @@ public class EntitiesExporter {
             }
             writer.write(sb.toString());
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb;
+    }
+
+    public static StringBuilder exportProtocolToTextFile(Transcript transcript, Protocol protocol, File file) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            sb.append("Protocol");
+            sb.append(transcript.getName());
+            sb.append("\n");
+            sb.append(protocol.getText());
+            writer.write(sb.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
