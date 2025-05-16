0584a791c679ce9fdabf2185390109421c9ebd09
Fatomartyr <choppyiney@gmail.com>
2025-05-16 13:23:45 +0300

    feature/LLM-integration: Adds TaskExtractor and adds LLMService to devide responsibilities between LLM configureand tasks that LLM does

--- app/src/main/java/logic/protocol/TextSummarizer.java
+++ app/src/main/java/logic/protocol/TextSummarizer.java
@@ -1,13 +1,11 @@
 package logic.protocol;
 
-import de.kherud.llama.InferenceParameters;
-import de.kherud.llama.LlamaModel;
-import de.kherud.llama.LlamaOutput;
-import de.kherud.llama.ModelParameters;
 import logic.general.Replica;
 import logic.general.Transcript;
 
 public class TextSummarizer {
+
+    private static final String MODEL_PATH = "dynamic-resources/Vikhr-Llama-3.2-1B-Q8_0.gguf";
 
     public static void summarize(Transcript transcript) {
         StringBuilder textSB = new StringBuilder();
@@ -15,10 +13,6 @@ public class TextSummarizer {
             textSB.append(replicas.getText());
         }
         String inputText = textSB.toString();
-
-        ModelParameters mp = new ModelParameters()
-                .setModel("dynamic-resources/Vikhr-Llama-3.2-1B-Q8_0.gguf");
-        LlamaModel llama = new LlamaModel(mp);
 
         String prompt = String.format("""
             Ты — русский языковой помощник.
@@ -33,20 +27,17 @@ public class TextSummarizer {
             ПЕРЕСКАЗ:
             """, inputText);
 
-        InferenceParameters ip = new InferenceParameters("")
-                .setPrompt(prompt)
-                .setIgnoreEos(false)
-                .setMinP(0.06f)
-                .setTopP(0.92f)
-                .setPenalizeNl(true)
-                .setRepeatPenalty(1.05f)
-                .setTemperature(0.25f);
-
-        StringBuilder summary = new StringBuilder();
-        for (LlamaOutput tok : llama.generate(ip)) {
-            summary.append(tok.text);
-        }
-        System.out.println(summary.toString().trim());
-        llama.close();
+        LLMService service = new LLMService(MODEL_PATH);
+        String summary = service.generate(
+                prompt,
+                false,
+                0.06f,
+                0.92f,
+                true,
+                1.05f,
+                0.25f
+        );
+        System.out.println(summary);
     }
+
 }
