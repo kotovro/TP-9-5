cac4bcca912858abe6ba6aa09d5a4afaf9a2d68f
kotovro <kotvoro@gmail.com>
2025-05-17 21:06:51 +0300

    feature/text-export Refactors transcript resulting export format

--- app/src/main/java/logic/utils/EntitiesExporter.java
+++ app/src/main/java/logic/utils/EntitiesExporter.java
@@ -32,8 +32,9 @@ public class EntitiesExporter {
     public static StringBuilder exportProtocolToTextFile(Transcript transcript, Protocol protocol, File file) {
         StringBuilder sb = new StringBuilder();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            sb.append("Protocol");
-            sb.append(transcript.getName());
+            sb.append("Protocol for transcript ").append(transcript.getName());
+            sb.append(" ");
+            sb.append(transcript.getDate());
             sb.append("\n");
             sb.append(protocol.getText());
             writer.write(sb.toString());
