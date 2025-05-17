08c7c986ec7286d51ae061243a9cd7a9eb4ee765
Paramon2003 <fgqwertyui1234@yandex.ru>
2025-05-17 15:32:05 +0300

    feature/develop-menu add base

--- app/src/main/java/ui/MainWindow.java
+++ app/src/main/java/ui/MainWindow.java
@@ -1,29 +1,39 @@
 package ui;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainWindow {
+public class MainWindow extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        setStage(stage);
+        stage.setResizable(false);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
     public static Scene getScene() {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoadStenogrammApp.class.getResource("/fx_screens/mainWindow.fxml"));
-        Scene scene = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/fx_screens/base.fxml"));
         try {
-            scene = new Scene(fxmlLoader.load(), 1137, 778);
+            return new Scene(fxmlLoader.load(), 1137, 778);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //String stylesheet = LoadStenogrammApp.class.getResource("/styles/load.css").toExternalForm();
-        //scene.getStylesheets().add(stylesheet);
-        return scene;
     }
 
     public static void setStage(Stage stage) {
         stage.setScene(getScene());
-        stage.setResizable(false);
-        stage.setTitle("Главное окно");
+        stage.setTitle("Встречеслав");
     }
 }
