27ecd74704a4f9ad09474ca16d13bfadd892ecd4
blacklavilass <vita20041129@yandex.ru>
2025-05-30 15:01:56 +0300

    make app executing after closing, add to system tray

--- app/src/main/java/ui/MainWindow.java
+++ app/src/main/java/ui/MainWindow.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,17 +9,73 @@ import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class MainWindow extends Application {
     public static final Font manropeBold = Font.loadFont(MainWindow.class.getResourceAsStream("/fonts/Manrope-Bold.ttf"), 12);
+    private TrayIcon trayIcon;
+    private Stage primaryStage;
 
     @Override
     public void start(Stage stage) throws IOException {
+        primaryStage = stage;
         setStage(stage);
         stage.setResizable(false);
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
+        setupTrayIcon(stage);
         stage.show();
+
+        stage.setOnCloseRequest(e -> {
+            e.consume();
+            stage.hide();
+        });
+        Platform.setImplicitExit(false);
+    }
+
+    private void setupTrayIcon(Stage stage) {
+        if (!SystemTray.isSupported()) {
+            System.out.println("Трей не поддерживается!");
+            return;
+        }
+
+        SystemTray tray = SystemTray.getSystemTray();
+        java.awt.Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/tray.png"));
+
+        PopupMenu popup = new PopupMenu();
+        MenuItem openItem = new MenuItem("Открыть");
+        MenuItem exitItem = new MenuItem("Выход");
+
+        ActionListener listener = e -> {
+            if (e.getSource() == openItem) {
+                Platform.runLater(stage::show);
+            } else if (e.getSource() == exitItem) {
+                Platform.exit();
+                tray.remove(trayIcon);
+            }
+        };
+
+        openItem.addActionListener(listener);
+        exitItem.addActionListener(listener);
+        popup.add(openItem);
+        popup.add(exitItem);
+
+        trayIcon = new TrayIcon(image, "Встречеслав", popup);
+        trayIcon.setImageAutoSize(true);
+
+        trayIcon.addActionListener(e -> Platform.runLater(() -> {
+            if (primaryStage != null) {
+                primaryStage.show();
+                primaryStage.toFront();
+            }
+        }));
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.err.println("Не удалось добавить иконку в трей");
+        }
     }
 
     public static void main(String[] args) {
